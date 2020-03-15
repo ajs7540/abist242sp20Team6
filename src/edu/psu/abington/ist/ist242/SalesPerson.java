6bdc1957dbc2b9363f3d152b065c57f013d9b4ec
Tom <tgressen@gmail.com>
2020-03-15 19:15:27 -0400

    Removed Hello

--- src/edu/psu/abington/ist/ist242/SalesPerson.java
+++ src/edu/psu/abington/ist/ist242/SalesPerson.java
@@ -1,3 +1,5 @@
+package edu.psu.abington.ist.ist242;
+
 public class SalesPerson {
 
     int salesID;
@@ -6,34 +8,22 @@ public class SalesPerson {
     public void setSalesPerson(String fname, String lname, int salesID , String sPass)
 
         {
-
             this.firstName = fname;
-
             this.lastName = lname;
-
             this.salesID =salesID;
-
             this.salesPassword = sPass;
-
-
         }
 
         public String getSalesPassword() {
-
             return salesPassword;
-
         }
 
         public String getFirstName() {
-
             return firstName;
-
         }
 
         public String getLastName() {
-
             return lastName;
-
         }
 
 }
