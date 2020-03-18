c6e51e561bec94dd01dddb70bf0251978b21f0b1
Tom <tgressen@gmail.com>
2020-03-18 19:12:29 -0400

    Added logic to user class. Added some accounts with checking auth

--- src/edu/psu/abington/ist/ist242/Main.java
+++ src/edu/psu/abington/ist/ist242/Main.java
@@ -6,13 +6,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Car Dealership Program!");
         System.out.println("Please select from the following options");
+
         System.out.println("Enter 1 to Login");
         System.out.println("Enter q to Quit");
-
         int a = Menu.callmenu(1);
         if(a == 1){
-            System.out.println("Please login");
-        }
+            for(int i = 0; i < 3; i++){
+                System.out.println("Please login");
+                String username;
+                String password;
 
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println("Username: ");
+                username = sc.nextLine();
+
+                System.out.println("Password: ");
+                password = sc.nextLine();
+
+                User login = new User(username, password);
+
+                if (login.checkPassword()) {
+                    System.out.println("You are logged in!");
+                    i = 3;
+                } else {
+                    System.out.println("The username and password you entered are incorrect.\nYou have " + (2 - i) + " more attempts until program quits.");
+                }
+            }
+        }
     }
 }
--- src/edu/psu/abington/ist/ist242/User.java
+++ src/edu/psu/abington/ist/ist242/User.java
@@ -1,4 +1,21 @@
 package edu.psu.abington.ist.ist242;
 
 public class User {
+
+    private String username;
+    private String password;
+    private String[][] accounts = {{"anthony", "supernova"},{"steve", "java1"}};
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean checkPassword()
+    {
+        if((username.equals(accounts[0][0])) && (password.equals(accounts[0][1])) || (username.equals(accounts[1][0])) && (password.equals(accounts[1][1])))
+            return true;
+        else
+            return false;
+    }
 }
