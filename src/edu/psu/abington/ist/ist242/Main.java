ee633ea9dab308c9a5891950000708bc187c0e4d
Tom <tgressen@gmail.com>
2020-03-18 13:07:28 -0400

    added rest of the classes

--- src/edu/psu/abington/ist/ist242/Main.java
+++ src/edu/psu/abington/ist/ist242/Main.java
@@ -4,39 +4,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String operation = "";
-        while (operation != "q") {
+        System.out.println("Welcome to Car Dealership Program!");
+        System.out.println("Please select from the following options");
+        System.out.println("Enter 1 to Login");
+        System.out.println("Enter q to Quit");
 
-            //checks for input valadation(IS 1,2,q???)
-            System.out.println("Enter 1 for login as customer");
-            System.out.println("Enter 2 for login as sales person");
-            System.out.println("Enter 3 register");
-            System.out.println("Enter q to quit");
-            while (!sc.hasNext("[1, 2, 3, q]")) {
-                System.out.println("That's not a valid operation");
-                operation = sc.next();
-            }
-
-
-            if (operation.equals("1")) {
-                System.out.println("Enter UserName:");
-                String customerUserName = sc.next();
-                System.out.println("Enter password:");
-                String customerPassword = sc.next();
-                if (Customer.checkUserNames(customerUserName )) {
-                }
-                else if (operation.equals("2")) {
-                    if (customerPassword == Customer.checkUserPassword()) {
-
-                    }
-
-                } else if (operation.equals("q")) {
-                    break;
-                }
-
-
-            }
+        int a = Menu.callmenu(1);
+        if(a == 1){
+            System.out.println("Please login");
         }
+
     }
 }
