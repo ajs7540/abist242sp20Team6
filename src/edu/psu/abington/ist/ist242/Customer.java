75051217a0fb90caa73a84850f668d0e4fdeba5d
sadykova-lilya <sadykova.lilya@gmail.com>
2020-03-25 17:40:32 -0400

    updated user / sales person/ customer

--- src/edu/psu/abington/ist/ist242/Customer.java
+++ src/edu/psu/abington/ist/ist242/Customer.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Customer extends User {
     int creditScore;
 
-    public Customer(int userID, String userName, String passWord, String eMail, String Address, int creditScore, boolean isSalesman, String username, String password) {
-        super(userID, userName, passWord, eMail, Address, username, password);
+    public Customer(int userID, String userName, String passWord, String eMail, String Address, int creditScore) {
+        super(userID, userName, passWord, eMail, Address);
         this.creditScore = creditScore;
+    }
+
+    public void addCustomer(int userID, String userName, String passWord, String eMail, String Address, int creditScore)
+    {
+        new Customer(userID, userName, passWord, eMail, Address, creditScore);
     }
 }
