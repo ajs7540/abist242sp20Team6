5951815d10d89d89480d4a7b9c31fa5252cce4d2
Tom <tgressen@gmail.com>
2020-03-26 12:58:10 -0400

    updated order class

--- src/edu/psu/abington/ist/ist242/PaymentInformation.java
+++ src/edu/psu/abington/ist/ist242/PaymentInformation.java
@@ -1,3 +1,12 @@
+/*
+Project: Car Dealership Team 6
+Purpose Details: A Application to manage a car dealership and customers buying cars.
+Course: IST242
+Author: Tom Fadelsak, William Perez, Amal Sabirov, Liliya Sadykova
+Date Developed: 3/12/20
+Last Date Changed:
+Revision: 0
+*/
 package edu.psu.abington.ist.ist242;
 import java.util.Date;
 public class PaymentInformation {
